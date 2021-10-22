50321165035c8c6bfbad88d32fc9dfd7ccbb6801
Piwko <marcinpiwko7@gmail.com>
2021-10-22 14:18:44 +0200

    added postgres sql

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -126,12 +126,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
